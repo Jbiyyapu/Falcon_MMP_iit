8b7057fe1d5ed610fbf839a30eacad45592be889
Ramesh Munaga <Ramesh Munaga@DESKTOP-CGBKO83.home>
2019-02-28 18:09:42 -0500

    Pushing the code from local repository to Github repository

---
+++ src/main/java/org/iit_mmp/mmp_iit1/App.java
@@ -0,0 +1,13 @@
+package org.iit_mmp.mmp_iit1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
